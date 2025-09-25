5d99c9dc2f1d8b25fae453ab12bfe5aab81bfc6e
FirassHossreh <firass.hus@gmail.com>
2025-09-25 16:13:07 +0300

    delete the react icon

--- PMM.ui/src/App.tsx
+++ PMM.ui/src/App.tsx
@@ -1,13 +1,9 @@
 import "./App.css";
-import { DatePicker } from "antd";
-import { CgAddR } from "react-icons/cg";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold">
-        <DatePicker />
-        <CgAddR />
-      </h1>
+      <h1 className="text-3xl font-bold">hello</h1>
     </>
   );
 }
