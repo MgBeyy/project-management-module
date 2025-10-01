c5474ae8b0b45d173dd3c21b0b8580ef1b2e39cc
zaid <zaidalmoughrabi@gmail.com>
2025-10-01 11:10:29 +0300

    User CRUD

---
+++ PMM.API/Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿
+using AutoWrapper.Wrappers;
+using Microsoft.AspNetCore.Mvc;
+using PMM.Core.DTOs;
+using PMM.Core.Forms;
+using PMM.Core.Services;
+
+namespace PMM.API.Controllers
+{
+    public class UserController : _BaseController
+    {
+        private readonly IUserService _userService;
+        public UserController(ILogger<UserController> logger, IUserService userService) : base(logger)
+        {
+            _userService = userService;
+        }
+        [ProducesResponseType(typeof(List<UserDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpPost()]
+        public async Task<ApiResponse> Create(CreateUserForm form)
+        {
+            await _userService.AddUserAsync(form);
+            return new ApiResponse("User created successfully", StatusCodes.Status201Created);
+        }
+        [ProducesResponseType(typeof(List<UserDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpGet()]
+        public async Task<ApiResponse> GetAll()
+        {
+            var users = await _userService.GetAllUsers();
+            return new ApiResponse(users, StatusCodes.Status200OK);
+        }
+        [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpGet("{userId:int}")]
+        public async Task<ApiResponse> GetUserById(int userId)
+        {
+            var user = await _userService.GetUserAsync(userId);
+            return new ApiResponse(user, StatusCodes.Status200OK);
+        }
+        [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpPut("{userId:int}")]
+        public async Task<ApiResponse> EditUser(CreateUserForm form, int userId)
+        {
+            var user = await _userService.EditUserAsync(userId, form);
+            return new ApiResponse(user, StatusCodes.Status200OK);
+        }
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpDelete("{userId:int}")]
+        public async Task<ApiResponse> DeleteUserAsync(int userId)
+        {
+            await _userService.DeleteUserAsync(userId);
+            return new ApiResponse("User deleted successfully", StatusCodes.Status200OK);
+        }
+    }
+}
---
+++ PPM.Core/Services/UserService.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Extensions.Logging;
+using PMM.Core.DTOs;
+using PMM.Core.Exceptions;
+using PMM.Core.Forms;
+using PMM.Core.Mappers;
+using PMM.Core.Validators;
+using PMM.Data.Repositories;
+using System.Security.Principal;
+
+namespace PMM.Core.Services
+{
+    public interface IUserService
+    {
+        Task<UserDto> AddUserAsync(CreateUserForm form);
+        Task<UserDto> GetUserAsync(int userId);
+        Task<UserDto> EditUserAsync(int userId, CreateUserForm form);
+        Task DeleteUserAsync(int userId);
+        Task<List<UserDto>> GetAllUsers();
+    }
+    public class UserService : _BaseService, IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly ILogger<UserService> _logger;
+        public UserService(IUserRepository userRepository,
+            ILogger<UserService> logger,
+            IPrincipal principal)
+            : base(principal, logger)
+        {
+            _userRepository = userRepository;
+            _logger = logger;
+        }
+
+        public async Task<UserDto> AddUserAsync(CreateUserForm form)
+        {
+            if (form == null)
+                throw new ArgumentNullException($"{typeof(CreateUserForm)} is empty");
+
+            var validation = FormValidator.Validate(form);
+            if (!validation.IsValid)
+                throw new BusinessException(validation.Errors);
+
+            var user = UserMapper.Map(form);
+            _userRepository.Create(user);
+            await _userRepository.SaveChangesAsync();
+            return UserMapper.Map(user);
+        }
+
+        public async Task DeleteUserAsync(int userId)
+        {
+            var user = await _userRepository.GetByIdAsync(userId);
+            if (user == null)
+                throw new NotFoundException("User Bulunamadı!");
+            _userRepository.Delete(user);
+            await _userRepository.SaveChangesAsync();
+        }
+
+        public async Task<UserDto> EditUserAsync(int userId, CreateUserForm form)
+        {
+            if (form == null)
+                throw new ArgumentNullException($"{typeof(CreateUserForm)} is empty");
+
+            var validation = FormValidator.Validate(form);
+            if (!validation.IsValid)
+                throw new BusinessException(validation.Errors);
+
+            if (userId == null)
+                throw new ArgumentNullException("userId Boş Olamaz!");
+            var user = await _userRepository.GetByIdAsync(userId);
+            if (user == null)
+                throw new NotFoundException("User Bulunamadı!");
+
+            user.Name = form.Name != null ? form.Name : user.Name;
+            user.Email = form.Email != null ? form.Email : user.Email;
+
+            _userRepository.Update(user);
+            await _userRepository.SaveChangesAsync();
+            return UserMapper.Map(user);
+        }
+
+        public async Task<List<UserDto>> GetAllUsers()
+        {
+            var users = _userRepository.QueryAll();
+            return UserMapper.Map(users.ToList());
+        }
+
+        public async Task<UserDto> GetUserAsync(int userId)
+        {
+            var user = await _userRepository.GetByIdAsync(userId);
+            if (user == null)
+                throw new NotFoundException("User Bulunamadı!");
+            return UserMapper.Map(user);
+        }
+    }
+}
