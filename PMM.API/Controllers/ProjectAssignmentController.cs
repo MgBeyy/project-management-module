1ada4dbb93235435f110bd66bc0efc2682c1fef1
zaid <zaidalmoughrabi@gmail.com>
2025-10-02 10:43:16 +0300

    project assignment

---
+++ PMM.API/Controllers/ProjectAssignmentController.cs
@@ -0,0 +1,60 @@
+﻿using AutoWrapper.Wrappers;
+using Microsoft.AspNetCore.Mvc;
+using PMM.Core.DTOs;
+using PMM.Core.Forms;
+using PMM.Core.Services;
+
+namespace PMM.API.Controllers
+{
+    public class ProjectAssignmentController : _BaseController
+    {
+        private readonly IProjectAssignmentService _projectAssignmentService;
+        public ProjectAssignmentController(
+            ILogger<ProjectAssignmentController> logger,
+            IProjectAssignmentService projectAssignmentService) : base(logger)
+        {
+            _projectAssignmentService = projectAssignmentService;
+        }
+
+        [ProducesResponseType(typeof(ProjectAssignmentDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpPost()]
+        public async Task<ApiResponse> Create(CreateProjectAssignmentForm form)
+        {
+            var assignment = await _projectAssignmentService.AddProjectAssignmentAsync(form);
+            return new ApiResponse(assignment, StatusCodes.Status200OK);
+        }
+
+        [ProducesResponseType(typeof(List<ProjectAssignmentDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpGet()]
+        public async Task<ApiResponse> GetAll()
+        {
+            var assignments = await _projectAssignmentService.GetAllProjectAssignments();
+            return new ApiResponse(assignments, StatusCodes.Status200OK);
+        }
+
+        [ProducesResponseType(typeof(ProjectAssignmentDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpGet("{projectAssignmentId:int}")]
+        public async Task<ApiResponse> GetById(int projectAssignmentId)
+        {
+            var assignment = await _projectAssignmentService.GetProjectAssignmentAsync(projectAssignmentId);
+            return new ApiResponse(assignment, StatusCodes.Status200OK);
+        }
+
+        [ProducesResponseType(typeof(ProjectAssignmentDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpPut("{projectAssignmentId:int}")]
+        public async Task<ApiResponse> EditProjectAssignment(CreateProjectAssignmentForm form, int projectAssignmentId)
+        {
+            var assignment = await _projectAssignmentService.EditProjectAssignmentAsync(projectAssignmentId, form);
+            return new ApiResponse(assignment, StatusCodes.Status200OK);
+        }
+    }
+}
---
+++ PPM.Core/Forms/CreateProjectAssignmentForm.cs
@@ -0,0 +1,19 @@
+﻿using PMM.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace PMM.Core.Forms
+{
+    public class CreateProjectAssignmentForm
+    {
+        [Required]
+        public int ProjectId { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public EProjectAssignmentRole Role { get; set; } = EProjectAssignmentRole.Member;
+        public DateTime? StartedAt { get; set; }
+        public DateTime? EndAt { get; set; }
+        [Range(0, int.MaxValue, ErrorMessage = "Çalışılması beklenen toplam saat negatif bir değer alamaz!")]
+        public int? ExpectedHours { get; set; }
+    }
+}
