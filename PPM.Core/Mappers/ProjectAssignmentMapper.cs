206b6986d90a70f2bd73e9eab9c1cb78afa32622
zaid <zaidalmoughrabi@gmail.com>
2025-10-02 10:43:16 +0300

    project assignment

---
+++ PPM.Core/Mappers/ProjectAssignmentMapper.cs
@@ -0,0 +1,52 @@
+﻿using PMM.Core.DTOs;
+using PMM.Core.Forms;
+using PMM.Data.Entities;
+
+namespace PMM.Core.Mappers
+{
+    public class ProjectAssignmentMapper
+    {
+        public static ProjectAssignment Map(CreateProjectAssignmentForm form)
+        {
+            return new ProjectAssignment
+            {
+                ProjectId = form.ProjectId,
+                UserId = form.UserId,
+                Role = form.Role,
+                StartedAt = form.StartedAt,
+                EndAt = form.EndAt,
+                ExpectedHours = form.ExpectedHours,
+            };
+        }
+        public static ProjectAssignmentDto Map(ProjectAssignment pa)
+        {
+            return new ProjectAssignmentDto
+            {
+                Id = pa.Id,
+                ProjectId = pa.ProjectId,
+                UserId = pa.UserId,
+                Role = pa.Role,
+                StartedAt = pa.StartedAt,
+                EndAt = pa.EndAt,
+                SpentHours = pa.SpentHours,
+                ExpectedHours = pa.ExpectedHours,
+                CreatedAt = pa.CreatedAt,
+                CreatedById = pa.CreatedById,
+                UpdatedAt = pa.UpdatedAt,
+                UpdatedById = pa.UpdatedById,
+            };
+        }
+        public static List<ProjectAssignmentDto> Map(List<ProjectAssignment> pa)
+        {
+            return pa.Select(p => Map(p)).ToList();
+        }
+        public static ProjectAssignment Map(UpdateProjectAssignmentForm form, ProjectAssignment pa)
+        {
+            pa.Role = form.Role;
+            pa.StartedAt = form.StartedAt;
+            pa.EndAt = form.EndAt;
+            pa.ExpectedHours = form.ExpectedHours;
+            return pa;
+        }
+    }
+}
