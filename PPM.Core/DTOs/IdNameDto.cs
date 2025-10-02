2bb8ba1fb938546b3e5767fd5e9c9b0e348a762f
zaid <zaidalmoughrabi@gmail.com>
2025-10-02 10:42:11 +0300

    Id name dto

---
+++ PPM.Core/DTOs/IdNameDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PMM.Core.DTOs
+{
+    public class IdNameDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ PPM.Core/Mappers/IdNameMapper.cs
@@ -0,0 +1,19 @@
+﻿using PMM.Core.DTOs;
+
+namespace PMM.Core.Mappers
+{
+    public class IdNameMapper
+    {
+        public static IdNameDto? Map(int? id, string? name)
+        {
+            if (id is null)
+                return null;
+            else
+                return new IdNameDto
+                {
+                    Id = (int)id,
+                    Name = name
+                };
+        }
+    }
+}
