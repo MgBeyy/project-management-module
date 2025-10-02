44a17fe9f28a2fcb8ec698a9e671505851af7aff
zaid <zaidalmoughrabi@gmail.com>
2025-10-02 10:42:34 +0300

    detailed project

---
+++ PPM.Core/DTOs/DetailedProjectDto.cs
@@ -0,0 +1,35 @@
+﻿using PMM.Data.Enums;
+
+namespace PMM.Core.DTOs
+{
+    public class DetailedProjectDto
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Title { get; set; }
+        public DateTime? PlannedStartDate { get; set; }
+        public DateTime? PlannedDeadline { get; set; }
+        public int? PlannedHours { get; set; }
+        public DateTime? StartedAt { get; set; }
+        public DateTime? EndAt { get; set; }
+        public EProjectStatus Status { get; set; }
+        public EProjectPriority Priority { get; set; }
+
+        public int? ParentProjectId { get; set; }
+        public ProjectDto? ParentProject { get; set; }
+        //public ICollection<IdNameDto>? ChildProjects { get; set; }
+
+        public int? ClientId { get; set; }
+        public ClientDto? Client { get; set; }
+
+        //public ICollection<IdNameDto> Assignments { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public int? CreatedById { get; set; }
+        public IdNameDto? CreatedByUser { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+        public int? UpdatedById { get; set; }
+        public IdNameDto? UpdatedByUser { get; set; }
+    }
+}
