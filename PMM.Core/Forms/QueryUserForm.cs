cf5e0d79f59f62d36bddd7104da916867672b7a2
zaid <zaidalmoughrabi@gmail.com>
2025-10-06 10:49:29 +0300

    Fix - Moved QueryUserForm

---
+++ PMM.Core/Forms/QueryUserForm.cs
@@ -0,0 +1,11 @@
+﻿using PMM.Core.Common;
+
+namespace PMM.Core.Forms
+{
+    public class QueryUserForm : BasePaginationForm
+    {
+        public string? Search { get; set; }
+        public string? SortBy { get; set; }
+        public bool SortDesc { get; set; } = false;
+    }
+}
