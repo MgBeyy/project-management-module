b79cb1627f89de1d1e0c0493f71b825e1f91f61a
zaid <zaidalmoughrabi@gmail.com>
2025-10-15 16:38:01 +0300

    feat: implement auditable and soft deletable base entity with configuration

---
+++ PMM.Data/Contexts/ClassMaps/_BaseEntityTypeConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PMM.Data.Entities;
+
+namespace PMM.Data.Contexts.ClassMaps
+{
+    public abstract class _BaseEntityTypeConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : _BaseEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Id).ValueGeneratedOnAdd();
+
+            builder.Property(e => e.CreatedAt).IsRequired();
+            builder.Property(e => e.CreatedById).IsRequired();
+            builder.HasOne(e => e.CreatedByUser).WithMany().HasForeignKey(e => e.CreatedById).OnDelete(DeleteBehavior.Restrict);
+
+            builder.Property(e => e.UpdatedAt).IsRequired(false);
+            builder.Property(e => e.UpdatedById).IsRequired(false);
+            builder.HasOne(e => e.UpdatedByUser).WithMany().HasForeignKey(e => e.UpdatedById).OnDelete(DeleteBehavior.Restrict);
+
+            builder.Property(e => e.IsDeleted).IsRequired().HasDefaultValue(false);
+            builder.HasOne(e => e.DeletedByUser).WithMany().HasForeignKey(e => e.DeletedById).OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasQueryFilter(e => !e.IsDeleted);
+        }
+    }
+}
---
+++ PMM.Data/Entities/Interfaces/ISoftDeletable.cs
@@ -0,0 +1,9 @@
+﻿namespace PMM.Data.Entities.Interfaces
+{
+    public interface ISoftDeletable
+    {
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public int? DeletedById { get; set; }
+    }
+}
