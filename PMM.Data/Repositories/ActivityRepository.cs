62d5c88e0cefadf9851b310aab5a475a1de2c9c6
zaid <zaidalmoughrabi@gmail.com>
2025-10-24 11:29:17 +0300

    feat: implement conflict detection for user activities in ActivityService and ActivityRepository

--- PMM.Data/Repositories/ActivityRepository.cs
+++ PMM.Data/Repositories/ActivityRepository.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using PMM.Data.Contexts;
 using PMM.Domain.Entities;
@@ -10,6 +11,16 @@ namespace PMM.Data.Repositories
         public ActivityRepository(ApplicationDbContext context, ILogger<ActivityRepository> logger)
             : base(context, logger)
         {
+        }
+
+        public async Task<bool> HasConflictingActivityAsync(int userId, DateTime startTime, DateTime endTime, int? excludeActivityId = null)
+        {
+            var query = Query(a => a.UserId == userId && a.StartTime < endTime && a.EndTime > startTime);
+            if (excludeActivityId.HasValue)
+            {
+                query = query.Where(a => a.Id != excludeActivityId.Value);
+            }
+            return await query.AnyAsync();
         }
     }
 }
