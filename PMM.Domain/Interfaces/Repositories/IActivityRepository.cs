c68ced38e4f66e9ca1b4b1aae2872a2bd77a29dc
zaid <zaidalmoughrabi@gmail.com>
2025-10-24 11:29:17 +0300

    feat: implement conflict detection for user activities in ActivityService and ActivityRepository

--- PMM.Domain/Interfaces/Repositories/IActivityRepository.cs
+++ PMM.Domain/Interfaces/Repositories/IActivityRepository.cs
@@ -3,5 +3,6 @@ namespace PMM.Domain.Interfaces.Repositories
 {
     public interface IActivityRepository : _IBaseRepository<Activity>
     {
+        Task<bool> HasConflictingActivityAsync(int userId, DateTime startTime, DateTime endTime, int? excludeActivityId = null);
     }
 }
