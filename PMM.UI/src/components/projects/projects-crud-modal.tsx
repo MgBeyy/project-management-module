d566c2c8ac9ebc2c67dba58d42f2f9c157d17c00
bunyaminkorkut <krktbunyamin@gmail.com>
2025-11-04 10:49:14 +0300

    feat: add download functionality for project reports and integrate into CrudModal

--- PMM.UI/src/components/projects/projects-crud-modal.tsx
+++ PMM.UI/src/components/projects/projects-crud-modal.tsx
@@ -1,4 +1,4 @@
-import { Button, Divider } from "antd";
+import { Button, Divider, Dropdown } from "antd";
 import { useState } from "react";
 import type { ConfigProviderProps } from "antd";
 import {
@@ -13,6 +13,9 @@ import { useProjectsStore } from "@/store/zustand/projects-store";
 import { useNotification } from "@/hooks/useNotification";
 import { deleteProject } from "@/services/projects/delete-project";
 import { ProjectDto } from "@/types";
+import { DownOutlined } from "@ant-design/icons";
+import { getReport } from "@/services/projects/get-report";
+import { saveAs } from "file-saver";
 
 type SizeType = ConfigProviderProps["componentSize"];
 
@@ -22,8 +25,9 @@ export default function CrudModal() {
   const [modalMode, setModalMode] = useState<'create' | 'edit' | 'view'>('create');
   const [deleteModalVisible, setDeleteModalVisible] = useState(false);
   const [isDeleting, setIsDeleting] = useState(false);
-  const notification  = useNotification();
-  
+  const notification = useNotification();
+  const { filters } = useProjectsStore();
+  const [loadings, setLoadings] = useState<boolean[]>([false, false]);
   const { selectedProject, clearSelectedProject, triggerRefresh } = useProjectsStore();
 
   const handleCreateClick = () => {
@@ -39,7 +43,7 @@ export default function CrudModal() {
     setModalMode('edit');
     setProjectModalVisible(true);
   };
-  
+
   const handleViewClick = () => {
     if (!selectedProject || !selectedProject.code) {
       notification.warning("Uyarı", "Lütfen görüntülemek için bir proje seçin.");
@@ -69,7 +73,7 @@ export default function CrudModal() {
       setDeleteModalVisible(false);
       clearSelectedProject();
       triggerRefresh(); // Tabloyu yenile
-      
+
 
     } catch (error: any) {
       console.error("Proje silinirken hata:", error);
@@ -86,39 +90,68 @@ export default function CrudModal() {
     triggerRefresh();
   };
 
+  const downloadMenu = [
+    {
+      key: '1',
+      label: 'Excel 1',
+      onClick: () => {
+        getReport({ query: filters }).then(({ blob, fileName }) => { saveAs(blob, fileName); });
+      }
+    },
+    {
+      key: '2',
+      label: 'CSV 2',
+      onClick: () => { console.log('CSV export clicked'); }
+    },
+  ];
+
+
   return (
     <>
-      <div className="m-4 flex items-center justify-around rounded-3xl bg-[#F1F5FF] gap-4">
-        <span>Projeler</span>
-        <Divider type="vertical" className="h-6" />
-        <Button 
-          type="primary" 
-          icon={<AiOutlinePlus />} 
-          size={size} 
-          onClick={handleCreateClick}
-        />
-        <Button 
-          type="primary" 
-          icon={<AiOutlineEdit />} 
-          size={size}
-          onClick={handleUpdateClick}
-        />
-        <Button 
-          type="primary" 
-          icon={<AiOutlineEye />} 
-          size={size}
-          onClick={handleViewClick}
-        />
-        <Button
-          type="primary"
-          icon={<AiOutlineDelete />}
-          size={size}
-          onClick={handleDeleteClick}
-          loading={isDeleting}
-          danger
-        />
-      </div>
+      <div className="flex justify-between w-full">
+        <div className="m-4 flex items-center justify-around rounded-3xl bg-[#F1F5FF] gap-4">
+          <span>Projeler</span>
+          <Divider type="vertical" className="h-6" />
+          <Button
+            type="primary"
+            icon={<AiOutlinePlus />}
+            size={size}
+            onClick={handleCreateClick}
+          />
+          <Button
+            type="primary"
+            icon={<AiOutlineEdit />}
+            size={size}
+            onClick={handleUpdateClick}
+          />
+          <Button
+            type="primary"
+            icon={<AiOutlineEye />}
+            size={size}
+            onClick={handleViewClick}
+          />
+          <Button
+            type="primary"
+            icon={<AiOutlineDelete />}
+            size={size}
+            onClick={handleDeleteClick}
+            loading={isDeleting}
+            danger
+          />
+        </div>
+        <Dropdown.Button
+          icon={<DownOutlined />}
+          loading={loadings[0]}
+          menu={{ items: downloadMenu }}
+          onClick={() => downloadMenu[0].onClick()}
+          style={{ width: "auto" }}
+          className="m-4"
+        >
+          Dışa Aktar
+        </Dropdown.Button>
 
+
+      </div>
       <CreateProjectModal
         visible={projectModalVisible}
         onClose={() => setProjectModalVisible(false)}
@@ -134,5 +167,6 @@ export default function CrudModal() {
         projectTitle={selectedProject?.title}
       />
     </>
+
   );
 }
