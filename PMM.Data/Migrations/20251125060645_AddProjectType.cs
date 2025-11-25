a800e46016d5a5cbebac80d8087fd519f783ac18
zaid <zaidalmoughrabi@gmail.com>
2025-11-25 10:03:58 +0300

    feat: Add 'Type' column to Projects table and update model snapshot

---
+++ PMM.Data/Migrations/20251125060645_AddProjectType.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PMM.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProjectType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Projects",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Projects");
+        }
+    }
+}
