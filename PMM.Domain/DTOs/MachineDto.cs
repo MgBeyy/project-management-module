e717f27b37e8b4966629d0ad9adfa2ae1eae7b49
zaid <zaidalmoughrabi@gmail.com>
2025-12-25 16:05:22 +0300

    machine dto

---
+++ PMM.Domain/DTOs/MachineDto.cs
@@ -0,0 +1,23 @@
+using PMM.Domain.Enums;
+
+namespace PMM.Domain.DTOs
+{
+    public class MachineDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public decimal? HourlyCost { get; set; }
+        public string Currency { get; set; }
+        public decimal? PurchasePrice { get; set; }
+        public DateTime? PurchaseDate { get; set; }
+        public bool IsActive { get; set; }
+        public EMachineStatus Status { get; set; }
+        public int? CreatedById { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int? UpdatedById { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
