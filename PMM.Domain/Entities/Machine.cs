3d78f2f3145b53ee52d88367004808a7607ba6e4
zaid <zaidalmoughrabi@gmail.com>
2025-12-25 16:01:12 +0300

    machine entity

---
+++ PMM.Domain/Entities/Machine.cs
@@ -0,0 +1,18 @@
+using PMM.Domain.Enums;
+
+namespace PMM.Domain.Entities
+{
+    public class Machine : _BaseEntity
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public decimal? HourlyCost { get; set; }
+        public string Currency { get; set; }
+        public decimal? PurchasePrice { get; set; }
+        public DateTime? PurchaseDate { get; set; }
+        public bool IsActive { get; set; }
+        public EMachineStatus Status { get; set; }
+    }
+}
