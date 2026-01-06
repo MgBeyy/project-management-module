dd5c99ffd560ceb39e4481c435670eb86fd2c451
zaid <zaidalmoughrabi@gmail.com>
2026-01-06 13:52:26 +0300

    feat: introduce Machine entity with CRUD operations and add UsefulLife property via migration.

---
+++ PMM.Data/Migrations/20260105113507_AddUsefulLifeToMachine.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PMM.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUsefulLifeToMachine : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UsefulLife",
+                table: "Machines",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UsefulLife",
+                table: "Machines");
+        }
+    }
+}
