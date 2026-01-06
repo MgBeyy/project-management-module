fa945c2f66c320969fd807634cbc369a56ea6612
zaid <zaidalmoughrabi@gmail.com>
2026-01-06 17:39:33 +0300

    feat: Implement user management service with CRUD operations and add hourly rate and currency fields to the user entity.

---
+++ PMM.Data/Migrations/20260106143810_AddHourlyRateAndCurrencyToUser.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PMM.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHourlyRateAndCurrencyToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Currency",
+                table: "Users",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "HourlyRate",
+                table: "Users",
+                type: "numeric(18,2)",
+                precision: 18,
+                scale: 2,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Currency",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "HourlyRate",
+                table: "Users");
+        }
+    }
+}
